953fa2976501637e8d8eebba10412dffbc827de1
sosring <hiperlogi@gmail.com>
2023-04-22 11:12:42 +0530

    small css changes

--- components/section/stadium/features.vue
+++ components/section/stadium/features.vue
@@ -38,7 +38,7 @@
 
   .items-container {
     display: grid;
-    grid-template-columns: repeat( auto-fit, minmax(150px, 1fr) );
+    grid-template-columns: repeat( auto-fit, minmax(120px, 1fr) );
 
      li {
       gap: .5rem;
